58093b858bfa91e899c67ecfbe1f75f88951a6c5
Mangoale Reokeditswe <mangoalerb@gmail.com>
2024-09-02 01:27:53 +0200

    Initial commit

---
+++ greet.js
@@ -0,0 +1,6 @@
+function greet(name, language) {
+    if (language === 'english') return `Hello, ${name}`;
+    if (language === 'spanish') return `Hola, ${name}`;
+    if (language === 'french') return `Bonjour, ${name}`;
+    return `Hello, ${name}`;
+}
